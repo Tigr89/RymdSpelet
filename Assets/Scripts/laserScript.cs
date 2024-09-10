4b988862c9ab4e9358b2aa23ca650d35bea25e23
Linn <linnskaghammar@yahoo.se>
2024-09-10 15:55:15 +0200

    del 1 player & bullet

---
+++ Assets/Scripts/laserScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserScript : MonoBehaviour
+{
+    public float laserSpeed =5;
+    public int laserDamage =5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * laserSpeed * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float movementSpeed;
+    public int playerHealth;
+    public GameObject laserBullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, -4, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX;
+        inputX = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(Vector3.right * movementSpeed * inputX * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserBullet, transform.position, Quaternion.identity);
+            
+        }
+
+        if (transform.position.x >= 10)
+        {
+            
+            transform.position = new Vector3(-9, transform.position.y, transform.position.z);
+        }
+
+         if (transform.position.x <= -10)
+        {
+            transform.position = new Vector3(9, transform.position.y, transform.position.z);
+        }
+        
+
+    }
+}
