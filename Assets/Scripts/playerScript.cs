5053e6b8cd7b53aee301d535e065d40987187ccb
Arvid Sandblom <LargerPizza@gmail.com>
2024-09-13 12:01:57 +0200

    fuck you

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -21,3 +21,26 @@ public class playerScript : MonoBehaviour
         transform.Translate(Vector3.right * movementspeed * inputX * Time.deltaTime);
     }
 }
+public class laserscript : MonoBehaviour
+{
+    public float speed;
+    //start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The laser moves up at the desired speed.
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        //If the laser gets  out of bounds (which in my case was 8 in y-axis) it will destroy itself.
+        if (transform.position.y > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
