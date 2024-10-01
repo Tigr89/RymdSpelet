0f70083d744ac045ea8d2a7f0bd45d4fec49dfa7
Linn <linnskaghammar@yahoo.se>
2024-10-01 11:59:06 +0200

    meteor, speedLine

---
+++ Assets/Scripts/MeteorScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+    
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //variera rörelsen
+        transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+        //transform.Translate(new Vector3(Random.Range(-10, 10), -1, 0) * movementSpeed * Time.deltaTime);
+
+        if (transform.position.y <= -8.0f)
+        {
+            transform.position = new Vector3(Random.Range(-8, 8), 5.5f, 0);
+        }
+    }
+
+    //If two items collide this code will run.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            //Call on method in player to deal damage
+
+        }
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -7,15 +7,20 @@ public class spawner : MonoBehaviour
 {
 
     public int enemyCounter;
+    public int meteorCounter;
     public GameObject Enemy;
     public GameObject Player;
     public GameObject laserBullet;
+    public GameObject Meteor;
+    public GameObject speedLine;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EnemySpawner());
+        StartCoroutine(MeteorSpawner());
     }
 
     // Update is called once per frame
@@ -43,9 +48,29 @@ public class spawner : MonoBehaviour
             }
             
         }
-        
 
+   
 
     }
+    IEnumerator MeteorSpawner()
+    {
+        while (true)
+        {
+            //if (GameObject.FindWithTag("Player") != null)
+            if (enemyCounter == 2 && GameObject.FindWithTag("Player") != null)
+            {
+                Instantiate(Meteor, new Vector3(Random.Range(-8, 8), 5.5f, 0), Quaternion.identity);
+                meteorCounter++;
 
+                Debug.Log(meteorCounter);
+                yield return new WaitForSeconds(2);
+            }
+            else
+            {
+                yield return null;
+            }
+
+        }
+
+    }
 }
