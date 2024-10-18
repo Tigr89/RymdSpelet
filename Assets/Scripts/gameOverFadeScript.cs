0e2bc36cf9ef8b5c05266606dd50288bd0582e15
unknown <ht24cast@axevalla.fhsk.se>
2024-10-18 11:52:02 +0200

    fade UI

--- Assets/Scripts/gameOverFadeScript.cs
+++ Assets/Scripts/gameOverFadeScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class gameOverFadeScript : MonoBehaviour
 {
+    private TMP_Text gameOverScreen;
+    private Color fadeColour;
+
     // Start is called before the first frame update
     void Start()
     {
-        //FAADEEE
+        gameOverScreen = GetComponent<TMP_Text>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameOverScreen.color += fadeColour;
+        fadeColour.a = 0.001f;
     }
 }
