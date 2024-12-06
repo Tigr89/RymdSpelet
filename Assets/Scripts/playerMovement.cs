d2a0c6d96035b9c6f74543c11f97eaf3bdaea81a
Luni <24htluni@axevalla.fhsk.se>
2024-12-06 10:56:55 +0100

    123

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
     public float playerSpeed = 1.0f;
     public GameObject projectile;
+    public float cooldownTime = 0.5f;
+    private float lastTimeUsed;
+    public float projectileDamage = 25;
+    public GameObject enemyObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,14 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * playerSpeed * Time.deltaTime);
+        }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
@@ -23,9 +37,19 @@ public class playerMovement : MonoBehaviour
         {
             transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && Time.time > lastTimeUsed + cooldownTime)
         {
+            lastTimeUsed = Time.time;
             Instantiate(projectile, this.transform.position, this.transform.rotation);
         }
+        if (transform.position.x < -6.5f)
+        {
+            transform.position = new Vector3(8.65f, transform.position.y, 0);    
+        }
+        if (transform.position.x > 8.65f)
+        {
+            transform.position = new Vector3(-6.5f, transform.position.y, 0);
+        }
     }
+
 }
---
+++ Assets/enemyShip1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShip1 : MonoBehaviour
+{
+    public float enemySpeed = 1.0f;
+    public float enemyHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * enemySpeed * Time.deltaTime);
+    }
+}
