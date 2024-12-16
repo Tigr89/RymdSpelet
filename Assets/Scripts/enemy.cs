93bbe705ee864ae7a79a1ec883d98fe6c1b7d073
skeleium123 <H23Daha@axevalla.fhsk.se>
2024-12-16 16:02:17 +0100

    power-ups script klart

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,29 +1,46 @@
-using System.Collections;
 using UnityEngine;
-
-public class enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 
 {
-
     public float movementSpeed = 1;
-    public float EnemyHealth = 1;
-    public GameObject you;
+    public GameObject power_up;
+    public GameObject power_up1;
+    public GameObject power_up2;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(0, 12);
+        transform.position = new Vector2(0, 9);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
-    }
 
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        you.SetActive(false);
+        Destroy(gameObject);
+
+        int dropChance = 20;
+        int randomChance = Random.Range(0, 100);
+        Debug.Log(randomChance);
+        
+        if (randomChance > dropChance + 60)
+        {
+            Instantiate(power_up, this.transform.position, this.transform.rotation);
+        }
+        else if (randomChance > dropChance + 40)
+        {
+            Instantiate(power_up1, this.transform.position, this.transform.rotation);
+        }
+        else if (randomChance > dropChance + 20)
+        {
+            Instantiate(power_up2, this.transform.position, this.transform.rotation);
+        }
+
     }
-
-
 }
